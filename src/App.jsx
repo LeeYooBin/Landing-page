8b722b9ffb0f5c4dba665b98755a9b874a954ddc
LeeYooBin <santosroberdan25@gmail.com>
2023-05-18 16:05:21 -0300

    Adding and setting styled-components and GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyle from './styles/global-style';
 
-const App = () => <></>;
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <p>Hello world!</p>
+  </ThemeProvider>
+);
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,55 @@
+// const pixelToRem = (pixels) => `${pixels / 16}rem`;
+
+const theme = {
+  colors: {
+    space: '#0B1E8A',
+    spaceLight: '#3D68B2',
+    spaceDark: '#040327',
+    gray1: '#0D0E13',
+    gray2: '#2C2D3A',
+    gray3: '#60616F',
+    gray4: '#898A93',
+    gray5: '#D5D5DB',
+    mars: '#E85937',
+    marsLight: '#FF8C70',
+    marsDark: '#CF3F1D',
+    sun: '#F5D15F',
+    uranus: '#24FFE9',
+    jupiter: '#E00E34',
+    earth: '#0FDD86',
+    saturn: '#554AD7',
+    text: '#FFF',
+    background: '#04032C',
+    backgroundSection: 'linear-gradient(180deg, #040327 0%, #0D0E13 24.49%)',
+    backgroundForm: '#0E0D40',
+  },
+  fonts: {
+    display: '800 6.2rem/9.6rem "Heebo", sans-serif',
+    heading1: '700 3.2rem/4.8rem "Heebo", sans-serif',
+    heading2: '500 2.4rem/3.2rem "Heebo", sans-serif',
+    heading3: '400 2rem/2.4rem "Heebo", sans-serif',
+    mobileHead: '800 3.6rem/5rem "Heebo", sans-serif',
+    mobileText: '500 1.4rem/2.4rem "Heebo", sans-serif',
+    text0: '400 1.4rem/2.4rem "Heebo", sans-serif',
+    text1: '400 1.6rem/2.4rem "Heebo", sans-serif',
+    text2: '500 1.4rem/2rem "Heebo", sans-serif',
+    text3: '500 1.8rem/2.4rem "Heebo", sans-serif',
+    text4: '500 1.8rem/3.2rem "Heebo", sans-serif',
+  },
+};
+
+// fonts: {
+//   display: `800 ${pixelToRem(62)}/${pixelToRem(96)} "Heebo", sans-serif`,
+//   heading1: `700 ${pixelToRem(32)}/${pixelToRem(48)} "Heebo", sans-serif`,
+//   heading2: `500 ${pixelToRem(24)}/${pixelToRem(32)} "Heebo", sans-serif`,
+//   heading3: `400 ${pixelToRem(20)}/${pixelToRem(24)} "Heebo", sans-serif`,
+//   mobileHead: `800 ${pixelToRem(36)}/${pixelToRem(50)} "Heebo", sans-serif`,
+//   mobileText: `500 ${pixelToRem(14)}/${pixelToRem(24)} "Heebo", sans-serif`,
+//   text0: `400 ${pixelToRem(14)}/${pixelToRem(24)} "Heebo", sans-serif`,
+//   text1: `400 ${pixelToRem(16)}/${pixelToRem(24)} "Heebo", sans-serif`,
+//   text2: `500 ${pixelToRem(14)}/${pixelToRem(20)} "Heebo", sans-serif`,
+//   text3: `500 ${pixelToRem(18)}/${pixelToRem(24)} "Heebo", sans-serif`,
+//   text4: `500 ${pixelToRem(18)}/${pixelToRem(32)} "Heebo", sans-serif`,
+// },
+
+export default theme;

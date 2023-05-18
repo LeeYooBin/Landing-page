7280e1d633c0238d9f18bbdcd856fe30f6971984
LeeYooBin <santosroberdan25@gmail.com>
2023-05-18 16:14:46 -0300

    Setting jest and tetsing lib

---
+++ src/styles/render-theme.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { render } from '@testing-library/react';
+import theme from './theme';
+
+const renderTheme = (component) => render(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
+
+export default renderTheme;
---
+++ src/tests/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import '@testing-library/jest-dom/extend-expect';
+import { describe, it } from '@jest/globals';
+import renderTheme from '../styles/render-theme';
+import App from '../App';
+
+describe('App component', () => {
+  it('renders', () => {
+    renderTheme(<App />);
+  });
+});

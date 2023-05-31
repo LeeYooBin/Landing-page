1a5fca5364386aa90b609f809b44c84ac026ebb3
LeeYooBin <santosroberdan25@gmail.com>
2023-05-30 21:30:34 -0300

    Testing Heading component render

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // import '@testing-library/jest-dom/extend-expect';
 import { describe, it } from '@jest/globals';
-import renderTheme from '../styles/render-theme';
-import App from '../App';
+import renderTheme from './styles/render-theme';
+import App from './App';
 
 describe('App component', () => {
   it('renders', () => {

97559ca4332bd96308ff52fc774a56cebcc4a6aa
LeeYooBin <santosroberdan25@gmail.com>
2023-05-31 23:33:32 -0300

    Testing the Heading component

--- src/components/Heading/Heading.test.jsx
+++ src/components/Heading/Heading.test.jsx
@@ -1,11 +1,38 @@
-import { describe, it } from '@jest/globals';
+import { describe, it, expect } from '@jest/globals';
 import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import 'jest-styled-components';
 import Heading from '.';
 import renderTheme from '../../styles/render-theme';
 
-describe('<Heading />', () => {
-  it('Should render with default values', () => {
-    renderTheme(<Heading text="Texto" />);
-    screen.getByRole('heading', { name: 'Texto' });
+describe('Heading component', () => {
+  it('renders without errors', () => {
+    renderTheme(<Heading />);
+  });
+
+  it('renders the correct text when isMain is false', () => {
+    const { getByText } = renderTheme(<Heading text="Subheading" />);
+    expect(getByText('Subheading')).toBeInTheDocument();
+  });
+
+  it('renders the correct text when isMain is true', () => {
+    const { getByText } = renderTheme(<Heading text="Main Heading" isMain />);
+    expect(getByText('Main Heading')).toBeInTheDocument();
+  });
+
+  it('renders the default text when no text prop is provided', () => {
+    expect(screen.queryByText('')).toBeInTheDocument();
+  });
+
+  it('validates the prop types correctly', () => {
+    const consoleError = console.error;
+    // eslint-disable-next-line no-undef
+    console.error = jest.fn();
+
+    renderTheme(<Heading text={123} isMain="true" />);
+    expect(console.error).toHaveBeenCalledTimes(2);
+
+    console.error.mockRestore();
+    console.error = consoleError;
   });
 });
